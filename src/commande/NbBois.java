4025fcd0637e82cb6be4d1aca927f57155a82226
BLANC Nicolas <Nicolas.Blanc@bordeaux-inp.fr>
2021-11-08 10:28:15 +0100

    Dimensions et NbBois

--- src/commande/NbBois.java
+++ src/commande/NbBois.java
@@ -1,4 +1,15 @@
 package commande;
 
 public class NbBois {
+    private int nombre;
+
+    public NbBois(int nombre) {
+        if (nombre <= 0)
+            throw new IllegalArgumentException("Le nombre de bois doit être strictement positif");
+        this.nombre=nombre;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
 }
