699ddf7d50058185562b5cf8cfab6049ba0bc693
BLANC Nicolas <Nicolas.Blanc@bordeaux-inp.fr>
2021-11-29 12:20:56 +0100

    début algo (ratio ch4rligo)

--- src/commande/Algo.java
+++ src/commande/Algo.java
@@ -1,14 +1,40 @@
 package commande;
 
+import java.util.Arrays;
+import java.util.List;
 
-/*class Algo extends Dcoupe{
-    int benef;
+class Algo{
     Client client;
     Fournisseur fournisseur;
     Panneau panneau;
+    Planche planche;
 
-    //private Algo(){
+    private Algo(){
 
-    //}
+    }
 
-}*/
+    public optimise(List <Client> listC, List<Fournisseur> listF){
+        for (Client value : listC) {
+            //Ordonner selon la Longueur puis la largeur
+            // https://www.codejava.net/java-core/collections/sorting-arrays-examples-with-comparable-and-comparator
+        }
+
+        int i = 0;
+        int k = 0;
+        int j = 0;
+        for (Client value : listC) {
+            int coordL=0;
+
+            while(coordL<=listF.get(i).bois.get(k).dimensions.longueur){
+
+
+                coordL=coordL+value.bois(j).dimensions.longueur;
+            }
+        }
+
+
+
+        return listD;
+    }
+
+}
