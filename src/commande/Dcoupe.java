6fdbf89e9a8f82a511bdbbe0a0346c4dc487c4fe
Lucas Fauchereau <lucas.fauchereau@enseirb-matmeca.fr>
2021-12-09 11:39:55 +0100

    Avancement du XMLWrite et de la classe Dcoupe

--- src/commande/Dcoupe.java
+++ src/commande/Dcoupe.java
@@ -3,17 +3,25 @@ package commande;
 import java.util.ArrayList;
 
 public class Dcoupe implements Generable{
-    private int idFournisseur;
-    private int idPanneau;
-    private int idClient;
-    private int idPlanche;
-    private int x;
-    private int y;
+    public int idFournisseur;
+    public int idPanneau;
+    public int widthPanneau;
+    public int heightPanneau;
+    public int idClient;
+    public int idPlanche;
+    public int widthPlanche;
+    public int heightPlanche;
+    public int x;
+    public int y;
 
-    Dcoupe (int idFournisseur,int idPanneau,int idClient,int idPlanche,int x,int y){
+    Dcoupe (int idFournisseur,int idPanneau,int widthPanneau,int heightPanneau,int idClient,int idPlanche,int widthPlanche,int heightPlanche,int x,int y){
         this.idClient=idClient;
         this.idPanneau=idPanneau;
         this.idPlanche=idPlanche;
+        this.widthPanneau=widthPanneau;
+        this.heightPanneau=heightPanneau;
+        this.widthPlanche=widthPlanche;
+        this.heightPlanche=heightPlanche;
         this.idFournisseur=idFournisseur;
         this.x=x;
         this.y=y;
--- src/ui/XmlWrite.java
+++ src/ui/XmlWrite.java
@@ -34,16 +34,16 @@ public class XmlWrite {
                 decoupe=doc.createElement("decoupe");
 
                 client=doc.createElement("client");
-                // client.setAttribute("id", d.client.id.toString()); Récupérer les infos de découpe
+                // client.setAttribute("id", String.valueOf(d.idClient)); // Récupérer les infos de découpe
                 decoupe.appendChild(client);
 
                 fournisseur=doc.createElement("fournisseur");
-                // fournisseur.setAttribute("id", d.fournisseur.id.toString()); Récupérer les infos de découpe
+                // fournisseur.setAttribute("id", String.valueOf(d.idFournisseur)); // Récupérer les infos de découpe
                 decoupe.appendChild(fournisseur);
 
                 position=doc.createElement("position");
-                // position.setAttribute("x", d.x.toString()); Récupérer les infos de découpe
-                // position.setAttribute("y", d.y.toString()); Récupérer les infos de découpe
+                // position.setAttribute("x", String.valueOf(d.x)); Récupérer les infos de découpe
+                // position.setAttribute("y", String.valueOf(d.y)); Récupérer les infos de découpe
                 decoupe.appendChild(position);
 
                 firstElement.appendChild(decoupe);
@@ -54,6 +54,52 @@ public class XmlWrite {
                 Transformer t = TransformerFactory.newInstance().newTransformer();
                 t.setOutputProperty(OutputKeys.INDENT, "yes");
                 t.setOutputProperty(OutputKeys.METHOD, "xml");
+                t.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+                t.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(filename)));
+            }
+            catch(TransformerException e){
+                System.out.println(e.getMessage());
+            }
+            catch(IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        catch(ParserConfigurationException e){
+            System.out.println("Error instantiating the DocumentBuilder "+e);
+        }
+    }
+
+    public void writeSVG(String filename, ArrayList<Dcoupe> l){
+        Document doc;
+        Element rectangle = null;
+
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        try{
+            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        
+            doc = docBuilder.newDocument();
+            
+            Element firstElement = doc.createElement("svg");
+            firstElement.setAttribute("version", "1.1");
+            firstElement.setAttribute("width", "1.1");
+            firstElement.setAttribute("height", "1.1");
+
+            for (Dcoupe d : l) {
+                rectangle=doc.createElement("rectangle");
+                rectangle.setAttribute("x", String.valueOf(d.x));
+                rectangle.setAttribute("x", String.valueOf(d.y));
+                // rectangle.setAttributes("width", String.valueOf(d.width));
+                // rectangle.setAttributes("height", String.valueOf(d.height));
+
+                firstElement.appendChild(rectangle);
+            }
+
+            doc.appendChild(firstElement);
+            try{
+                Transformer t = TransformerFactory.newInstance().newTransformer();
+                t.setOutputProperty(OutputKeys.INDENT, "yes");
+                t.setOutputProperty(OutputKeys.METHOD, "svg");
                 t.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 
                 t.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(filename)));
